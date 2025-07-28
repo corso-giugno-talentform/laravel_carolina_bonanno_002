c1a9377aed3786ae3dddc1afb0478c1b3cd71dc0
carob98 <caro.bon98@gmail.com>
2025-07-28 13:06:40 +0200

    Update con carousel overlay

--- resources/views/components/heroSection.blade.php
+++ resources/views/components/heroSection.blade.php
@@ -1,8 +1,10 @@
 <div id="carouselExampleAutoplaying" class="position-relative carousel slide" data-bs-ride="carousel">
-    <div class="position-absolute z-3 top-50 start-50 translate-middle">
-        <div class="d-flex flex-column align-items-center justify-content-center">
-            <h1 class="title-hero text-white fst-italic">Velvet Frame</h1>
-            <h2 class="text-white fw-light">Le tue news sulla moda</h2>
+    <div class="position-absolute z-2 overlay">
+        <div class="position-absolute z-3 top-50 start-50 translate-middle">
+            <div class="d-flex flex-column align-items-center justify-content-center align-self-center">
+                <h1 class="title-hero text-white fst-italic">Velvet Frame</h1>
+                <h2 class="text-white fw-light">Le tue news sulla moda</h2>
+            </div>
         </div>
     </div>
     <div class="carousel-inner">
@@ -16,12 +18,12 @@
             <img src="images/style.webp" class="d-block w-100" alt="Slide 3">
         </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+    <button class="carousel-control-prev z-3" type="button" data-bs-target="#carouselExampleAutoplaying"
         data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+    <button class="carousel-control-next z-3" type="button" data-bs-target="#carouselExampleAutoplaying"
         data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
