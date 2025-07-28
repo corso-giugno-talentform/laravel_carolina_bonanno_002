2786c1db56fd61e9c6385b209d576fc9c48d88ed
carob98 <caro.bon98@gmail.com>
2025-07-28 09:44:15 +0200

    Rename variabile dettaglio

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-bg-color">
     <div class="container">
         <a class="navbar-brand" href="{{ route('homepage') }}">Velvet Frame</a>
-        @if (!$notDetail)
+        @if ($isDetail)
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
                 aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
