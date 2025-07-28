7b80a36f867df9ea6a8e18cb77fb16f1c321639a
carob98 <caro.bon98@gmail.com>
2025-07-28 12:44:37 +0200

    Inserimento npm & vite

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -3,9 +3,11 @@
     <x-slot name='nav'><x-navbar :isDetail="true" :isHidden="false"></x-navbar></x-slot>
     <article class="container.fluid">
         <img class="img-fluid w-100 h-100 object-fit-cover"src="{{ $article['img'] }}" alt="{{ $article['alt'] }}">
-        <div class="m-5 d-flex flex-column">
-            <h1 class="text-center title">{{ $article['title'] }}</h1>
-            <p class="py-5">{{ $article['paragraph'] }}</p>
+        <div class="m-5 d-flex flex-column p-5">
+            <small class="text-center mb-4">Categoria: <b>{{ $article['category'] }}</b></small>
+            <h1 class="text-center title mb-5">{{ $article['title'] }}</h1>
+            <p class="mb-4">{{ $article['paragraph'] }}</p>
+            <small class="px-2">Articolo di: <b>{{ $article['author'] }}</b></small>
         </div>
     </article>
 </x-main>
--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -5,8 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? 'Velvet Frame' }}</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
